53a44b249ef910b57b4550641b6d8d8d9046143a
Ciaran Liedeman <ciaran@stackworx.io>
2022-08-21 22:34:40 +0200

    chore: testing embed

---
+++ docs/src/components/StorybookEmbed/index.tsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+
+const baseStorybookUrl = "https://aquamarine-choux-75c766.netlify.app";
+
+function storyUrl(group: string, name: string) {
+  const url = new URL(baseStorybookUrl + "/iframe.html");
+  url.searchParams.append("args", "");
+  url.searchParams.append("id", `${group}-${name}`);
+  url.searchParams.append("viewMode", "story");
+  return url.href;
+}
+
+interface Props {
+  group?: string;
+  name: string;
+}
+
+export function StorybookEmbed({ group = "core", name }: Props) {
+  // TODO border
+  const src = React.useMemo(() => storyUrl(group, name), [group, name]);
+  return <iframe width="100%" height="300px" src={src}></iframe>;
+}
