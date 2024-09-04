00765f68833acf5c2519a89138338bddb6711b37
pieterventer <pieter@stackworx.io>
2024-09-04 09:42:09 +0200

    chore: date time picker progress

--- packages/x-date-pickers/src/DateTimePicker.tsx
+++ packages/x-date-pickers/src/DateTimePicker.tsx
@@ -1,9 +1,10 @@
 import {
   FieldValues,
-  Path,
   useController,
   UseControllerProps,
   useFormContext,
+  RegisterOptions,
+  FieldPath,
 } from 'react-hook-form';
 import {
   DateTimePicker as MuiDateTimePicker,
@@ -14,9 +15,9 @@ import { PickerValidDate } from '@mui/x-date-pickers/models';
 
 type DateTimePickerProps<
   TDate extends PickerValidDate,
+  TFieldValues extends FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
   TEnableAccessibleFieldDOMStructure extends boolean = false,
-  TName extends Path<TFieldValues> = Path<FieldValues>,
-  TFieldValues extends FieldValues = FieldValues,
 > = Omit<
   MuiDateTimePickerProps<TDate, TEnableAccessibleFieldDOMStructure>,
   'value'
@@ -24,32 +25,34 @@ type DateTimePickerProps<
   UseControllerProps<TFieldValues, TName>;
 
 export function DateTimePicker<
-  TDate,
+  TDate extends PickerValidDate,
+  TFieldValues extends FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
   TEnableAccessibleFieldDOMStructure extends boolean = false,
-  TName extends Path<TFieldValues> = Path<FieldValues>,
-  TFieldValues extends FieldValues = FieldValues,
 >({
   name,
   rules,
   ...props
 }: DateTimePickerProps<
   TDate,
-  TEnableAccessibleFieldDOMStructure,
+  TFieldValues,
   TName,
-  TFieldValues
+  TEnableAccessibleFieldDOMStructure
 >) {
   const { slotProps, ...otherPickerProps } = props;
 
-  const { textField, ...otherSlotProps } = slotProps;
-
   const { setError, clearErrors, control } = useFormContext();
+
   const {
     field: { onChange, value, ref, onBlur },
     fieldState,
   } = useController({
     name,
     control,
-    rules,
+    rules: rules as unknown as Omit<
+      RegisterOptions<FieldValues, TName>,
+      'disabled' | 'valueAsNumber' | 'valueAsDate' | 'setValueAs'
+    >,
   });
 
   return (
@@ -57,50 +60,43 @@ export function DateTimePicker<
       onChange={onChange}
       value={value}
       slotProps={{
-        slotProps: {
-          textField: {
-            inputRef: ref,
-            error: !!fieldState.error,
-            helperText: fieldState.error.message ?? textField.helperText ?? ' ',
-            onBlur,
-            ...textField,
-          },
-          ...otherSlotProps,
+        ...slotProps,
+        textField: {
+          ...slotProps?.textField,
+          inputRef: ref,
+          error: !!fieldState.error,
+          onBlur,
+          helperText:
+            //@ts-expect-error incomplete typing
+            fieldState.error?.message ?? slotProps?.textField?.helperText,
         },
       }}
       onError={(reason) => {
+        console.log({ reason });
         switch (reason) {
           // TODO: set Error for all available reasons
           // and set up a corresponding story component for each one in storybook
           case 'invalidDate':
             setError(name, { message: 'Invalid date' });
             break;
-
           case 'disablePast':
             setError(name, { message: 'Values in the past are not allowed' });
             break;
-
           case 'disableFuture':
             setError(name, { message: 'Values in the past are not allowed' });
             break;
-
           case 'maxDate':
             setError(name, {
               type: 'max',
-              message: `Date should not be after ${format(props.maxDate, 'P')}`,
+              message: `Date should not be after ${props?.maxDate ? format(props.maxDate, 'P') : ''}`,
             });
             break;
-
           case 'minDate':
             setError(name, {
               type: 'min',
-              message: `Date should not be before ${format(
-                props.minDate,
-                'P'
-              )}`,
+              message: `Date should not be before ${props?.minDate ? format(props.minDate, 'P') : ''}`,
             });
             break;
-
           case 'shouldDisableDate':
             // TODO
             // shouldDisableDate returned true, render custom message according to the `shouldDisableDate` logic
--- src/stories/DateTimePicker.stories.tsx
+++ src/stories/DateTimePicker.stories.tsx
@@ -1,7 +1,6 @@
 import Stack from '@mui/material/Stack';
 import { StoryFn, Meta } from '@storybook/react';
 import { useForm, FormProvider } from 'react-hook-form';
-
 import { DateTimePicker } from '../../packages/x-date-pickers/src/DateTimePicker';
 import { Form } from './Form';
 
@@ -38,5 +37,139 @@ export const Default = {
 
   args: {
     label: 'Default',
+  },
+};
+
+export const required = {
+  render: Template,
+
+  args: {
+    label: 'Required',
+    rules: { required: true },
+  },
+};
+
+export const InvalidDate = {
+  render: Template,
+
+  args: {
+    label: 'Invalid Date',
+    slotProps: {
+      textField: {
+        helperText: 'Start typing to show error...',
+      },
+    },
+  },
+};
+
+export const DisablePast = {
+  render: Template,
+
+  args: {
+    label: 'Disable Past',
+  },
+};
+
+export const DisableFuture = {
+  render: Template,
+
+  args: {
+    label: 'Disable Future',
+  },
+};
+
+export const MaxDate = {
+  render: Template,
+
+  args: {
+    label: 'Max Date',
+  },
+};
+
+export const MinDate = {
+  render: Template,
+
+  args: {
+    label: 'Min Date',
+  },
+};
+
+export const MaxDateTime = {
+  render: Template,
+
+  args: {
+    label: 'Max Date Time',
+  },
+};
+
+export const MinDateTime = {
+  render: Template,
+
+  args: {
+    label: 'Min Date Time',
+  },
+};
+
+export const ShouldDisableDate = {
+  render: Template,
+
+  args: {
+    label: 'Should Disable Date',
+  },
+};
+
+export const MaxTime = {
+  render: Template,
+
+  args: {
+    label: 'Max Time',
+  },
+};
+
+export const MinutesStep = {
+  render: Template,
+
+  args: {
+    label: 'Minutes Step',
+  },
+};
+
+export const ShouldDisableMonth = {
+  render: Template,
+
+  args: {
+    label: 'Should Disable Month',
+  },
+};
+
+export const ShouldDisableTimeHours = {
+  render: Template,
+
+  args: {
+    label: 'Should Disable Time Hours',
+  },
+};
+
+export const ShouldDisableTimeMinutes = {
+  render: Template,
+
+  args: {
+    label: 'Should Disable Time Minutes',
+  },
+};
+
+export const ShouldDisableTimeSeconds = {
+  render: Template,
+
+  args: {
+    label: 'Should Disable Time Seconds',
+  },
+};
+
+export const ShouldDisableYear = {
+  render: Template,
+
+  args: {
+    label: 'Should Disable Year',
   },
 };
